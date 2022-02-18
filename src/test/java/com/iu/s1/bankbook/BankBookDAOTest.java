7070c013916734b03c52bf6bcd5454766cff062a
Daeki <kimdaeki@gmail.com>
2022-02-18 16:08:38 +0900

    study2

--- src/test/java/com/iu/s1/bankbook/BankBookDAOTest.java
+++ src/test/java/com/iu/s1/bankbook/BankBookDAOTest.java
@@ -14,42 +14,44 @@ public class BankBookDAOTest extends MyJunitTest {
 	@Autowired
 	private BankBookDAO bankBookDAO;
 
-	@Test
+	//@Test
 	public void check() {
 		assertNotNull(bankBookDAO);
 	}
 
 	//List
-	@Test
+	//@Test
 	public void listTest() throws Exception {
 		 List<BankBookDTO> ar = bankBookDAO.list();
 		 assertNotEquals(0, ar.size());
 	}
 	
 	//Insert
-	//@Test
+	@Test
 	public void addTest()throws Exception{
-		BankBookDTO bankBookDTO = new BankBookDTO();
-		bankBookDTO.setBookName("t2");
-		bankBookDTO.setBookContents("c2");
-		bankBookDTO.setBookRate(3.12);
-		bankBookDTO.setBookSale(1);
-		int result = bankBookDAO.add(bankBookDTO);
-		
-		assertEquals(1, result);
+		for(int i=0;i<10;i++) {
+			BankBookDTO bankBookDTO = new BankBookDTO();
+			bankBookDTO.setBookName("bookName"+i);
+			bankBookDTO.setBookContents("Contents"+i);
+			bankBookDTO.setBookRate(1.12+i);
+			bankBookDTO.setBookSale(1);
+			int result = bankBookDAO.add(bankBookDTO);
+		}
+		System.out.println("Insert Finish");
+		//assertEquals(1, result);
 	}
 	
 	//Detail
-	@Test
+	//@Test
 	public void detailTest()throws Exception{
 		BankBookDTO bankBookDTO = new BankBookDTO();
 		bankBookDTO.setBookNumber(150L);
-		bankBookDTO = bankBookDAO.detail(2L);
+		bankBookDTO = bankBookDAO.detail(bankBookDTO);
 		assertNotNull(bankBookDTO);
 	}
 	
 	//Delete
-	@Test
+	//@Test
 	public void deleteTest()throws Exception{
 		BankBookDTO bankBookDTO = new BankBookDTO();
 		bankBookDTO.setBookNumber(2L);
